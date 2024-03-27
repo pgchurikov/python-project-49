6dc6b671e01f3c0c6225634108bcf16cc9066854
Pavel Churikov <p.g.churikov@gmail.com>
2024-03-27 20:14:48 +0300

    prompt problem

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from cli import welcome_user
+
 
 def main():
     print('Welcome to the Brain Games!')
@@ -7,3 +9,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    welcome_user()
