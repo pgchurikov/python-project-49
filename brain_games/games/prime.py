e9a73f156982f9af072fc0566ab3ecda77d6e743
Pavel Churikov <p.g.churikov@gmail.com>
2024-04-16 21:49:34 +0300

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,7 @@ import math
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2, int(math.sqrt(number))+1):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
     return True
